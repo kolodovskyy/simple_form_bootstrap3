1dfe7e2dab62e4918787b0f53727c75b1494c463
Yuriy Kolodovskyy <kolodovskyy@ukrindex.com>
2014-09-08 13:09:55 +0300

    input_html option for date and datetime

--- lib/simple_form/inputs/date_time_picker_input.rb
+++ lib/simple_form/inputs/date_time_picker_input.rb
@@ -8,7 +8,9 @@ module SimpleForm
       private
 
       def input_html_options
-        if options.key? :value
+        if options.key?(:input_html) && options[:input_html].key?(:value)
+          value = options[:input_html][:value]
+        elsif options.key? :value
           value = options[:value]
         elsif object.respond_to?(attribute_name)
           value = object.send(attribute_name)
@@ -19,7 +21,13 @@ module SimpleForm
         value = Time.zone.parse(value) if value.is_a?(String)
         value = value.strftime("%Y-%m-%d %H:%M") if value.respond_to?(:strftime)
 
-        { class: 'form-control text-center datetime datetimepicker', value: value.presence || '' }
+        result = { class: 'form-control text-center datetime datetimepicker', value: value.presence || '' }
+        if options.key? :input_html
+          result[:class] += ' ' + options[:input_html][:class] if options[:input_html][:class].present?
+          result.merge(options[:input_html].except(:class, :value))
+        else
+          result
+        end
       end
     end
   end
